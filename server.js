f0cbfd79816c75966d7cb76012e7ad4275ab5237
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-11-23 14:26:03 +0530

    disconnect function added

--- server.js
+++ server.js
@@ -1,13 +1,60 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const { Server } = require('socket.io')
+const { Server } = require('socket.io');
+const ACTIONS = require('./src/Actions');
 const server = http.createServer(app)
 const io = new Server(server);
 
+// map all the joined user's socket id with username in this userSocketMap object
+const userSocketMap = {}
+
+function getAllConnectedClients(roomId) {
+    // return all list of clients connected in a room (in array format so use array.from to convert it to array)
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
+        return {
+            socketId,
+            username: userSocketMap[socketId],
+        }
+    })
+}
 
 io.on('connection', (socket) => {
     console.log('socket connected', socket.id);
+
+    // get the parameter sent from EditorPage.js
+    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+        userSocketMap[socket.id] = username;
+        // if first time creating room 
+        socket.join(roomId);
+        // if already some user are availabe in the room then get list of clients connected in the room
+        const clients = getAllConnectedClients(roomId);
+        console.log(clients); //log in terminal -> all the clients connected in a room 
+
+        // send this information to frontend(EditorPage.js) to update the ui of editor page
+        clients.forEach(({ socketId }) => {
+            io.to(socketId).emit(ACTIONS.JOINED, {
+                clients,
+                username,
+                socketId: socket.id
+            })
+        })
+    })
+
+    // for disonnection
+    socket.on('disconnecting', () => {
+        // get all socket rooms
+        const rooms = [...socket.rooms]; //you can you Array.from also to convert it to an array or also this syntax is correct (spread method)
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+                socketId: socket.id,
+                username: userSocketMap[socket.id],
+            })
+        })
+        // delete from map
+        delete userSocketMap[socket.id];
+        socket.leave(); //leave the room
+    })
 })
 
 
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -4,29 +4,76 @@ import Editor from '../components/Editor';
 import { initSocket } from '../socket';
 import ACTIONS from '../Actions';
 import { useLocation } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { useNavigate, Navigate, useParams } from 'react-router-dom';
 function EditorPage() {
 
     const socketRef = useRef(null);
     const location = useLocation();
+    const reactNavigator = useNavigate()
+    const { roomId } = useParams(); // because in app.js we set path as /editor/:roomId
+
+    const [clients, setClients] = useState([]);
 
     useEffect(() => {
         const init = async () => {
             socketRef.current = await initSocket();
 
-            // for sending the room id
+            // for handling errors 
+            socketRef.current.on('connect_error', (err) => handleErrors(err));
+            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+            function handleErrors(e) {
+                console.log('socket error', e);
+                toast.error('Socket connection failed , try again later.');
+                reactNavigator('/');
+            }
+
+            // for sending the room id and listen this on server.js
             socketRef.current.emit(ACTIONS.JOIN, {
                 roomId,
                 username: location.state?.username,
             });
+
+
+            // Listening for joined event when someone joins the room
+            socketRef.current.on(ACTIONS.JOINED, ({ clients, username, socketId }) => {
+                if (username !== location.state?.username) {
+                    // dont notify when myself joins the room ok
+                    toast.success(`${username} joined the room.`)
+                    console.log(`${username} joined`);
+                }
+                setClients(clients);
+            })
+
+            //Listening for disconnected
+            socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+                toast.success(`${username} left the room.`)
+                // remove the entry from clients array 
+                setClients((prev) => {
+                    return prev.filter((client) => client.socketId !== socketId);
+                })
+            })
+
         };
         init();
+
+        // clearing the listeners to avoid memory leak
+        return () => {
+            socketRef.current.off(ACTIONS.JOINED);
+            socketRef.current.off(ACTIONS.DISCONNECTED);
+            socketRef.current.disconnect();
+        }
+
     }, [])
 
-    const [clients, setClients] = useState([
-        { socketId: 1, username: "arvind" },
-        { socketId: 2, username: "prakash" },
-        { socketId: 3, username: "deepak" },
-    ]);
+
+
+    // if username or roomid not found then navigate to home page using Navigate hook
+    if (!location.state) {
+        <Navigate to="/" />
+    }
+
 
     return (
         <div className='mainWrap'>
