b76c85f9cf4d5da8552eb4945528e04bc795c6a4
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-11-23 23:41:50 +0530

    final done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -68,7 +68,7 @@ function Home() {
                 </div>
             </div>
             <footer>
-                <h4>Built by IT</h4>
+                {/* <h4>Built by IT</h4> */}
             </footer>
         </div>
     )
